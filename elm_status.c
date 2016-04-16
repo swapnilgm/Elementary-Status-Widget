b145d63aaec4f61db543d6cbf8b37d2fb7678201
swapnilgm <swapnilgmhamane@gmail.com>
2016-04-16 11:29:07 +0530

    photo selector and status editing incomplete

--- elm_status.c
+++ elm_status.c
@@ -24,6 +24,13 @@ typedef struct
 	VISIBILITY visibility;
 	char * picture;
 	Evas_Object* icon;
+	/*file selector*/
+	Evas_Object* fs;
+	const char *fs_title; 
+	/*file selector window*/
+	Evas_Object* fsw;
+	Evas_Object* entry;
+	Eina_Bool editing_mode;
 }Elm_Status_Data;
 
 /* smart callbacks coming from elm button objects (besides the ones
@@ -58,6 +65,131 @@ _elm_status_eo_base_destructor(Eo *obj, Elm_Status_Data *pd)
 
 }
 
+	static Eina_Bool
+_selection_done(void *data, const Eo_Event *event)
+{
+	Elm_Status_Data *sd = data;
+	const char *file = event->event_info;
+	Evas_Object *del;
+
+	if (file){
+		elm_image_file_set(sd->icon, file, NULL);
+		//TODO add event callback
+		sd->picture= file;
+
+	}
+
+	del = sd->fsw;
+	sd->fs = NULL;
+	sd->fsw = NULL;
+	evas_object_del(del);
+//	eo_event_callback_call
+//		(sd->obj, ELM_FILESELECTOR_EVENT_FILE_CHOSEN, (void *)file);
+
+	return EINA_TRUE;
+}
+
+
+	static void 
+_new_window_add(Elm_Status_Data *pd)
+{
+	Evas_Object *win, *bg;
+
+	win = elm_win_add(NULL, "imageselector", ELM_WIN_DIALOG_BASIC);
+	elm_win_title_set(win, "Change image");
+	elm_win_autodel_set(win, EINA_TRUE);
+	eo_event_callback_add(win, ELM_WIN_EVENT_DELETE_REQUEST, _selection_done, pd);
+	bg = elm_bg_add(win);
+	evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	elm_win_resize_object_add(win, bg);
+	pd->fsw = win;
+	evas_object_show(bg);
+	_initialize_image_selector(pd);
+	evas_object_show(win);
+}
+
+
+
+	void
+_initialize_image_selector(Elm_Status_Data *pd)
+{
+
+	EINA_LOG_WARN("Adding new iwndow ");
+	printf("%x", pd->fsw);
+	/* add file selector, in list mode */ 
+	pd->fs = elm_fileselector_add(pd->fsw);
+	//elm_widget_mirrored_automatic_set(pd->fs, EINA_FALSE);
+	// elm_fileselector_expandable_set(pd->fs, EINA_TRUE);
+	/* enable the fs file name entry */ 
+	//elm_fileselector_is_save_set(pd->fs, EINA_TRUE);
+	evas_object_size_hint_weight_set(pd->fs, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(pd->fs, EVAS_HINT_FILL, EVAS_HINT_FILL);
+	eo_event_callback_add(pd->fs, ELM_FILESELECTOR_EVENT_DONE, _selection_done, pd);
+
+	evas_object_show(pd->fs);
+	elm_win_resize_object_add(pd->fsw, pd->fs);
+
+	//	evas_object_hide(pd->fs;
+
+}
+
+
+/**
+ * Private function Just to manage initial image loading parameters
+ *
+ */
+	static void
+_initialize_image(Eo *obj, Elm_Status_Data *pd)
+{
+	//use icon widget to load image 
+	pd->icon = elm_icon_add(obj);
+	evas_object_repeat_events_set(pd->icon, EINA_TRUE);
+	//although the icon widget provides basic image functionality
+	//the apis over icon are deprecated 
+	//so we use image api to manage the image attributes
+	elm_image_resizable_set(pd->icon, EINA_TRUE, EINA_TRUE);
+	elm_image_smooth_set(pd->icon, EINA_TRUE);
+	elm_image_fill_outside_set(pd->icon, EINA_TRUE);
+	elm_image_prescale_set(pd->icon, 0);
+	elm_object_scale_set(pd->icon, elm_widget_scale_get(obj));
+	//evas_object_smart_callback_add(pd->icon, "clicked", _new_window_add,NULL);
+
+	//TODO 
+	elm_icon_standard_set(pd->icon, "no_photo");
+	pd->picture = "no_photo";
+	//_initialize_image_selector(obj, pd);
+
+}
+
+
+static void 
+_status_clicked_cb(void *data, Evas_Object *obj, const char * emission, const char* source)
+{
+	Elm_Status_Data *pd =data;
+	printf("*****************");
+	
+	Evas_Object *txtObj = evas_object_name_find(obj,"elm.status.text.text");
+	printf("%x", txtObj);
+	if(!pd->editing_mode)
+	{
+		pd->editing_mode = EINA_TRUE;
+		evas_object_show(pd->entry);
+	evas_object_hide(txtObj);
+	} else {
+		
+		pd->editing_mode = EINA_FALSE;
+	evas_object_show(txtObj);
+		evas_object_hide(pd->entry);
+	}
+}
+
+	static void
+	_initialize_entry(Eo * obj, Elm_Status_Data *pd)
+{
+pd->entry = elm_entry_add(obj);
+evas_object_hide(pd->entry);
+	}
+
 	EOLIAN static void
 _elm_status_evas_object_smart_add(Eo *obj, Elm_Status_Data *pd)
 {
@@ -79,28 +211,17 @@ _elm_status_evas_object_smart_add(Eo *obj, Elm_Status_Data *pd)
 	else
 		EINA_LOG_DBG("Layout theme set to base.");
 
-	//use icon widget to load image 
-	pd->icon = elm_icon_add(obj);
-	evas_object_repeat_events_set(pd->icon, EINA_TRUE);
-	//although the icon widget provides basic image functionality
-	//the apis over icon are deprecated 
-	//so we use image api to manage the image attributes
-	elm_image_resizable_set(pd->icon, EINA_TRUE, EINA_TRUE);
-	elm_image_smooth_set(pd->icon, EINA_TRUE);
-	elm_image_fill_outside_set(pd->icon, EINA_TRUE);
-	elm_image_prescale_set(pd->icon, 0);
-	elm_object_scale_set(pd->icon, elm_widget_scale_get(obj));
+	_initialize_image(obj,pd);
 
-	//	char *picture =  "/duel_boot.jpeg";
-	elm_icon_standard_set(pd->icon, "no_photo");
-	//	if(!elm_image_file_set(pd->icon, picture, NULL))
-	//	{
-	//		EINA_LOG_WARN("could not set the image." );
-	//	}
-	//	else 
-	pd->picture = "no_photo";
+	Evas_Object *txtObj = evas_object_name_find(wd->resize_obj,"elm.status.text.text");
+	printf("asfgfggfdsdfsd%x", txtObj);
+	if(!pd->editing_mode)
+	//edje_object_part_swallow(wd->resize_obj,"elm.picture.selector",pd->fs);
 	edje_object_part_swallow(wd->resize_obj,"elm.picture.image",pd->icon);
 
+	_initialize_entry(obj, pd);
+	edje_object_part_swallow(wd->resize_obj,"elm.status.text.entry",pd->entry);
+	edje_object_signal_callback_add(wd->resize_obj, "elm,action,click", "*",_status_clicked_cb, pd);
 	evas_object_size_hint_align_set(pd->icon, EVAS_HINT_FILL,
 			EVAS_HINT_FILL);
 	evas_object_size_hint_weight_set(pd->icon, EVAS_HINT_EXPAND,
@@ -113,10 +234,7 @@ _elm_status_evas_object_smart_add(Eo *obj, Elm_Status_Data *pd)
 _elm_status_evas_object_smart_del(Eo *obj, Elm_Status_Data *pd)
 {
 	EINA_LOG_WARN(__func__);
-	//evas_obj_smart_add(eo_super(obj, ELM_STATUS_CLASS));
-	//	ELM_SAFE_FREE(pd->image, evas_object_del);
-	//	eo_do_super(obj, ELM_STATUS_CLASS, evas_object_del);
-
+	evas_obj_smart_del(eo_super(obj, ELM_STATUS_CLASS));
 }
 
 	EOLIAN static Eina_Bool
@@ -206,7 +324,7 @@ elm_status_add(Evas_Object *parent)
 	EINA_SAFETY_ON_NULL_RETURN_VAL(parent, NULL);
 	Evas_Object *obj = eo_add(MY_CLASS, parent);
 	return obj;
-	}
+}
 
 	EOLIAN static void
 _elm_status_visibility_set(Eo *obj, Elm_Status_Data *pd, VISIBILITY visibility)
